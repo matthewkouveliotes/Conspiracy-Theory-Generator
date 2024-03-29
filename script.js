1a247f3bd1dd9d3924f74b81a503e1648a1f9042
Matthew Kouveliotes <160647852+matthewkouveliotes@users.noreply.github.com>
2024-03-28 23:27:33 -0400

    i am so cooked bro

--- script.js
+++ script.js
@@ -3,22 +3,32 @@ var events;
 var groups;
 var things;
 var formats;
-
+var check = 0;
 function loadPresets() {
     fetch("./resources/people.txt").then((res) => res.text()).then((text) => {
         people = text.split('\n');
+        check++;
+        calculatePossible(check);
     }).catch((e) => console.error(e));
     fetch("./resources/events.txt").then((res) => res.text()).then((text) => {
-            events = text.split('\n');
-        }).catch((e) => console.error(e));
+        events = text.split('\n');
+        check++;
+        calculatePossible(check);
+    }).catch((e) => console.error(e));
     fetch("./resources/groups.txt").then((res) => res.text()).then((text) => {
-            groups = text.split('\n');
-        }).catch((e) => console.error(e));
+        groups = text.split('\n');
+        check++;
+        calculatePossible(check);
+    }).catch((e) => console.error(e));
     fetch("./resources/things.txt").then((res) => res.text()).then((text) => {
-            things = text.split('\n');
-        }).catch((e) => console.error(e));
+        things = text.split('\n');
+        check++;
+        calculatePossible(check);
+    }).catch((e) => console.error(e));
     fetch("./resources/formats.txt").then((res) => res.text()).then((text) => {
         formats = text.split('\n');
+        check++;
+        calculatePossible(check);
     }).catch((e) => console.error(e));
 }
 var currSpin = 0;
@@ -59,4 +69,39 @@ function createConspiracy() {
 
     document.getElementById("content").innerHTML = text;
 
+}
+
+function calculatePossible(checks) {
+    if(checks != 5) return;
+    var total = 0;
+    var eventAmt = events.length;
+    var peopleAmt = people.length;
+    var groupAmt = groups.length;
+    var thingAmt = things.length;
+    for(var i = 0; i < formats.length; i++) {
+        var tots = [];
+        var text = formats[i];
+        var peopleN = 0;
+        var amtPeople = text.match(/_Person_/g);
+        if(amtPeople != null) peopleN = amtPeople.length;
+        var groupN = 0;
+        var amtGroup = text.match(/_Group_/g);
+        if(amtGroup != null) groupN = amtGroup.length;
+        var eventN = 0;
+        var amtEvent = text.match(/_Event_/g);
+        if(amtEvent != null) eventN = amtEvent.length;
+        var thingN = 0;
+        var amtThing = text.match(/_Thing_/g);
+        if(amtThing != null) thingN = amtThing.length;
+        for(var j = 0; j <peopleN; j++) tots.push(peopleAmt);
+        for(var j = 0; j <groupN; j++) tots.push(groupAmt);
+        for(var j = 0; j <eventN; j++) tots.push(eventAmt);
+        for(var j = 0; j <thingN; j++) tots.push(thingAmt);
+        var ta = 1;
+        for(var j = 0; j < tots.length; j++) ta*=tots[j];
+        total += ta;
+
+
+    }
+    document.getElementById("total").innerHTML = "There are " + total + " Possible Conspiracy Theories";
 }
